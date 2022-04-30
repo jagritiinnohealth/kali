85189282996b03e83f5cdeb6c3bc739f02942f4e
pramodkumaryadav <pramodyadav027@gmail.com>
2022-04-30 17:11:13 +0200

    add capabilites, url and driver factory

---
+++ src/main/java/ebloodbanking/factories/URLFactory.java
@@ -0,0 +1,27 @@
+package ebloodbanking.factories;
+
+import ebloodbanking.choices.Host;
+import ebloodbanking.config.EnvFactory;
+import com.typesafe.config.Config;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Slf4j
+public class URLFactory {
+    private static Config config = EnvFactory.getInstance().getConfig();
+
+    private URLFactory() {
+        throw new IllegalStateException("Static factory class");
+    }
+
+    public static URL getHostURL(Host host) {
+        log.info("Getting hostURL for Host: {}", host);
+        try {
+            return new URL(config.getString("HOST_URI"));
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException(String.format("%s is Malformed host URL.", config.getString("HOST_URI")), e);
+        }
+    }
+}
